cb99cbf77cd22237bbb29ff11a7cb0adfb362fcd
Ron Zhong <ron.zhong@mims.com>
2021-06-25 10:54:28 +0800

    [AcePointer] BlazorServer App Initialization

---
+++ AcePointer.NameSorter.Web/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿
+function countDown() {
+    $('#timer').html(--i);
+
+    if (i == 1) {
+        $('#loadingBar').hide(1000);
+    }
+
+    if (i == 0) {
+        location.href = './';
+    }
+}
+
+function MainLayoutLoaded() {
+    $('#blazor-host-ui').hide();
+    clearInterval(interval);
+}
